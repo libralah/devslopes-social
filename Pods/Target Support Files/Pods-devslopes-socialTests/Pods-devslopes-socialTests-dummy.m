e39ee40d6c0c1296dce44d33b66e0e2ab7238942
libralah <hungnguyen@Hungs-Mac-mini.fios-router.home>
2017-06-13 19:22:24 -0400

    Completed setting up Firebase integration

---
+++ Pods/Target Support Files/Pods-devslopes-socialTests/Pods-devslopes-socialTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_devslopes_socialTests : NSObject
+@end
+@implementation PodsDummy_Pods_devslopes_socialTests
+@end
---
+++ devslopes-social/SignInVC.swift
@@ -0,0 +1,21 @@
+//
+//  SignInVC.swift
+//  devslopes-social
+//
+//  Created by Hung Nguyen on 6/13/17.
+//  Copyright © 2017 Luvdub Nation. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
