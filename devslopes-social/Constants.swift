cbeea339ba10b9a20827e9d12e09f18b3188c212
libralah <hungnguyen@Hungs-Mac-mini.fios-router.home>
2017-06-14 06:42:12 -0400

    Sign in ui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Hung Nguyen on 6/14/17.
+//  Copyright © 2017 Luvdub Nation. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY : CGFloat = 120.0 / 255.0
